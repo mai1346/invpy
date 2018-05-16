7d3c0f8f80cd2e86f56dadbd947daaa9e6108833
mai1346 <37104892+mai1346@users.noreply.github.com>
2018-05-16 17:20:09 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: gbk -*-
+"""
+Created on Wed May 16 13:02:16 2018
+
+@author: mai1346
+"""
+
+from setuptools import setup
+
+setup(
+  name = 'invpy',
+  packages = ['invpy'], 
+  version = '0.0.1',
+  description = 'An experimental financial data scraper.',
+  author = 'Haoyuan Mai',
+  author_email = 'maihaoyuan@gmail.com',
+  url = 'https://github.com/mai1346/invpy'
+#  install_requires= ['requests','pandas','re','beautifulsoup4','numpy']
+)
+
